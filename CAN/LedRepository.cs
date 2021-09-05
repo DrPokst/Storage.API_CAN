ff72afa0c9f85d023d8db24e87eeac28d7393f0c
DrPokst <62307971+DrPokst@users.noreply.github.com>
2021-09-05 23:04:09 +0300

    Sutvarkyta LED klasė
    Būtina ištestuoti!

--- CAN/LedRepository.cs
+++ CAN/LedRepository.cs
@@ -9,58 +9,42 @@ namespace Storage.API.CAN
     {
         public async Task<bool> TurnOffAll()
         {
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync("http://192.168.8.98/api/Led/off/all"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    var result = JsonConvert.DeserializeObject<Byte[]>(apiResponse);
-                    return true;
-                }
-            }
-
+            var link = "http://192.168.8.98/api/Led/off/all";
+            await HttpRequest(link); 
+            return true;
         }
 
         public async Task<bool> TurnOffLed(int id)
         {
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync("http://192.168.8.98/api/Led/off" + "?id=" + id))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    var result = JsonConvert.DeserializeObject<Byte[]>(apiResponse);
-                    return true;
-                }
-            }
-
+            var link = "http://192.168.8.98/api/Led/off" + "?id=" + id;
+            await HttpRequest(link); 
+            return true;
         }
 
         public async Task<bool> TurnOnAll(string color)
         {
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync("http://192.168.8.98/api/Led/on/all" + "?color=" + color))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    var result = JsonConvert.DeserializeObject<Byte[]>(apiResponse);
-                    return true;
-                }
-            }
-
+            var link = "http://192.168.8.98/api/Led/on/all" + "?color=" + color;
+            await HttpRequest(link); 
+            return true;
         }
 
         public async Task<bool> TurnOnLed(int id, string color)
         {
+            var link = "http://192.168.8.98/api/Led/on" + "?id=" + id + "&color=" + color;
+            await HttpRequest(link); 
+            return true;
+        }
+
+        private async Task<bool> HttpRequest(string link){
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync("http://192.168.8.98/api/Led/on" + "?id=" + id + "&color=" + color))
+                using (var response = await httpClient.GetAsync(link))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
                     var result = JsonConvert.DeserializeObject<Byte[]>(apiResponse);
                     return true;
                 }
             }
-
         }
     }
 }
